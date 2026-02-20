dfd4d6fea1e8351be2ab51cc476bb1c0a55d0931
johnsonwane <johnsonwane@gmail.com>
2026-02-20 11:52:11 +0800

    Update schema.sql
    rename to oa_codex
    Create DB in https://oa.hahahaxinli.com/pma

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE DATABASE IF NOT EXISTS training_collab DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE training_collab;
+CREATE DATABASE IF NOT EXISTS oa_codex DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE oa_codex;
 
 -- =========================
 -- Drop tables (reverse order)
